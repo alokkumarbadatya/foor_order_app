51c59b17ceecf4d14f3d16439fe2de36cbf4312e
alokr <alokrock19s7@gmail.com>
2024-08-29 20:36:27 +0530

    Initial commit first food order app

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Food App',
+      title: 'Food  App',
       theme: ThemeData(
         primaryColor: primary,
         textTheme: GoogleFonts.latoTextTheme(
